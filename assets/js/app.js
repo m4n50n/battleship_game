ab1b081fb930587043967d9aec142add6b257444
m4n50n <josegarciarodriguez89@hotmail.com>
2022-01-07 10:26:14 +0000

    feat: add random ships positions and reestructure js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,21 @@
-/* Variables */
-// 0 = empty; 1 = part of a ship; 2 = a sunken part of a ship; 3 = a missed shot
-let game_board = [ 
+// #region GENERAL VARIABLES
+// Definitions of ships and their number of parts
+const ShipsDefinitions = {
+    cruiser: 6,
+    submarine: 5,
+    destroyer: 4,
+    carrier: 3,
+    frigate: 2
+}
+
+const DefaultPositions = 0;
+const RandomPositions = 1;
+
+/**
+ * Multidimensional array containing the square types (0 = Empty; 1 = Part of a ship; 2 = A sunken part of a ship; 3 = A missed shot)
+ * Each array corresponds to a row and each value of the array corresponds to a cell on the game board
+ */
+ let GameBoard = [ 
     [1,1,1,1,1,0,0,0,1],
     [0,0,0,0,0,0,0,0,1],
     [0,0,0,0,0,0,0,0,1],
@@ -12,135 +27,237 @@ let game_board = [
     [0,0,1,0,0,0,0,0,0]
 ];
 
-const game_board_container = document.querySelector("#game-board");
-const total_parts = game_board.map(rows => rows.filter(cell_value => cell_value ===1).length).reduce((x, y) => x +y);
-const remaining_parts = document.querySelector("#remaining-parts");
-const game_result = document.querySelector("#game-result");
-const fire_modal = document.querySelector("#fire-modal");
+// Square types and their CSS classes
+const emptySquare = 0;
+const shipPart = 1;
+const sunkenPart = 2;
+const missingShot = 3;
 
-const show_ships_button = document.querySelector(".show_ships");
-let showed_ships = false;
+const SquareStyles = {
+    0: "",
+    1: "parts",
+    2: "sunken",
+    3: "missed"
+}
 
-/* Listener */
-document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("square")) {
-        fire_torpedo(e.target);        
+// DOM elements
+const GameBoardContainer = document.querySelector("#game-board");
+const RemainingParts = document.querySelector("#remaining-parts");
+const GameUserNotifications = document.querySelector("#game-notifications");
+const ShowShipsButton = document.querySelector(".show_ships");
+
+const FireModal = document.querySelector("#fire-modal");
+const ModalXCoordinateInput = document.querySelector("#x-coordinate");
+const ModalYCoordinateInput = document.querySelector("#y-coordinate");
+const GameTypeModal = document.querySelector("#game-type-modal");
+
+// Controls
+let ShowedShips = false; // Control if the non-sunken parts of the ships are hidden or shown
+const TotalGameSquares = GameBoard.map(row => row.length).reduce((x, y) => x + y);
+const TotalShipsParts = Object.keys(ShipsDefinitions).map(ship => ShipsDefinitions[ship]).reduce((x, y) => x + y); // Sum of all parts of all ships. With reduce(), we obtain the sum of all values of the map array output
+// #endregion GENERAL VARIABLES
+
+// #region FUNCTIONS
+const InitApp = () => {
+    ShowModal(GameTypeModal, true);
+}
+
+const PlaceShipsPositions = (type) => {
+    if (type === RandomPositions) {
+        RestartGameBoardVariable();
+        PlaceRandomShips();
     }
-});
 
-/* Functions */
-const render_board = () => {
-    let board = `<div class="blank_guide"></div>`;
+    ShowModal(GameTypeModal, false);
+    RenderBoard();
+}
+
+const RestartGameBoardVariable = () => {
+    GameBoard = GameBoard.map(row => row.map(cell => 0)); // convert all values to 0
+}
+
+const PlaceRandomShips = () => {
+    /**
+     * Ships will be placed starting from a random row, cell and orientation
+     * We will consider that a ship fits if there is a consecutive number of free spaces (horizontally or vertically) equal to the size os the ship in the random obtained row and starting from the random obtained cell
+     */
+    for (const ship in ShipsDefinitions) {
+        const ShipWidth = ShipsDefinitions[ship];
+        let PlacedShip = false;
+
+        while (!PlacedShip) {
+            const ShipRenderInitRow = Math.floor(Math.random() * 9); // Random number from 0 to 8
+            const ShipRenderInitCell = Math.floor(Math.random() * 9); // Random number from 0 to 8
+            const ShipRenderOrientation = Math.floor(Math.random() * 2) + 1; // Random number from 1 to 2
+
+            const horizontal = 1;
+            const vertical = 2;
+
+            let ConsecutiveZerosCounter = 0;
+            let AvailablePlacesIndex = [];
+            let Available = false;
+            
+            if (ShipRenderOrientation === horizontal) {
+                let row = GameBoard[ShipRenderInitRow];
+                
+                for (let i = 0; i < row.length; i++) {
+                    if (i < ShipRenderInitCell) { continue; } // We will only check starting from the random obtained cell
+
+                    if (row[i] === 0) { // 0 = Empty square
+                        AvailablePlacesIndex.push(i);
+                        ConsecutiveZerosCounter++;
+
+                        if (ConsecutiveZerosCounter === ShipWidth) {
+                            Available = !Available;
+                            break; // Exit from loop
+                        }
+                    }
+                    else {
+                        AvailablePlacesIndex = [];
+                        ConsecutiveZerosCounter = 0;
+                    }
+                }
+
+                if (Available) {
+                    AvailablePlacesIndex.map(available_cell_index => {
+                        GameBoard[ShipRenderInitRow][available_cell_index] = ShipWidth;
+                    });
+
+                    PlacedShip = !PlacedShip;
+                }
+            }
+            else if (ShipRenderOrientation === vertical) { // With this feature, we will check if the boat fits on the vertical axis of GameBoard variable
+                for (i = 0; i < GameBoard.length; i++) {
+                    if (i < ShipRenderInitRow) { continue; } // We will only check starting from the random obtained row
+
+                    let CellValue = GameBoard[i][ShipRenderInitCell];
+                    if (CellValue === 0) { // 0 = Empty square
+                        AvailablePlacesIndex.push(i);
+                        ConsecutiveZerosCounter++;
+
+                        if (ConsecutiveZerosCounter === ShipWidth) {
+                            Available = !Available;
+                            break; // Exit from loop
+                        }
+                    }
+                    else {
+                        AvailablePlacesIndex = [];
+                        ConsecutiveZerosCounter = 0;
+                    }
+                }
+
+                if (Available) {
+                    AvailablePlacesIndex.map(available_row_index => {
+                        GameBoard[available_row_index][ShipRenderInitCell] = ShipWidth;
+                    });
+
+                    PlacedShip = !PlacedShip;
+                }                
+            }
+        }
+    }
+
+    console.log(GameBoard);
+}
+
+const RenderBoard = () => {
+    let board = `<div class="blank_guide"></div>`; // blank square from the upper left corner
     
     // Horizontal guides
     for (let i = 1; i <= 9; i++) {
         board += `<div class="h_guide">${i}</div>`;
     }
 
-    // Vertical guides and squares 
-    for (let y = 1; y < 10; y++) { // 9 rows under the horizontal guides
-        for (let x = 1; x <= 10; x++) { // Each row has one vertical guide + 9 squares
-            if (x === 1) {
-                board += `<div class="v_guide"><span>${y}</span></div>`;
+    /**
+     * Vertical guides and squares 
+     * - 9 rows under the horizontal guides
+     * - Each row has one vertical guide + 9 squares
+     */
+    GameBoard.map((row, y) => { // row = each GameBoard array; y = vertical axis (rows index)
+        row.map((squareType, x) => { 
+            if (x === 0) { // Vertical guide (first div of the row)
+                board += `<div class="v_guide"><span>${y + 1}</span></div>`; // Sum 1 because coordinates begins at 1 but the array indexes begins at 0
             }
-            else {
-                let status_value = game_board[y - 1][x - 2]; // Search coordinates in game_board values
-                let status_class = (status_value === 2) ? "sunken" : (status_value === 3) ? "missed" : "";
-                
-                let show_ship_class = (showed_ships && (status_value === 1 || status_value === 2)) ? "parts" : "";
+            
+            const StyleBoard = (squareType === sunkenPart || squareType === missingShot) ? squareType : emptySquare;
+            const showShipValue = (ShowedShips && (squareType === shipPart || squareType === sunkenPart)) ? shipPart : emptySquare;
+            board += `<div class="square ${SquareStyles[StyleBoard]} ${SquareStyles[showShipValue]}" onclick="FireShip(${y}, ${x})"></div>`;
+        });
+    });
 
-                board += `<div class="square ${status_class} ${show_ship_class}"></div>`;
-            }
-        }
-    }
-
-    game_board_container.innerHTML = board;
-    check_remaining_parts();
+    GameBoardContainer.innerHTML = board;
+    CheckRemainingParts();
 }
 
-const show_ships = () => {
-    if (showed_ships) {
-        showed_ships = false;
-        show_ships_button.innerHTML = "Show";
-    }
-    else {
-        showed_ships = true;
-        show_ships_button.innerHTML = "Hide";
+const CheckRemainingParts = () => {
+    const TotalSunkenParts = document.querySelectorAll(`.${SquareStyles[sunkenPart]}`).length;
+    const TotalMissedShots = document.querySelectorAll(`.${SquareStyles[missingShot]}`).length;
+    RemainingParts.innerHTML = TotalShipsParts - TotalSunkenParts;
+    
+    if ((TotalGameSquares - TotalShipsParts) === TotalMissedShots) {
+        SetUserNotifications("GAME OVER!!");
+        FinishedGame();
+    }    
+    else if (TotalShipsParts === TotalSunkenParts) {
+        SetUserNotifications("YOU WIN!!");
+        FinishedGame();
     }
     
-    render_board();
 }
 
-const fire_torpedo = (element) => {
-    shoot(get_coordinates(element));
-}
-
-const get_coordinates = (element) => {
-    let game_board_elements = game_board_container.children;
-    let element_index, x, y;
-
-    // Get the index of the element inside the container
-    element_index = Object.values(game_board_elements).indexOf(element).toString();
-
-    y = parseInt(element_index[0]); 
-    x = parseInt(element_index[1]);
-
-    return [y, x];
-}
-
-const shoot = (coordinates) => {
-    let y = coordinates[0] - 1; // Substract 1 because arrays index begin at 0
-    let x = coordinates[1] - 1;
-
-    let actual_status = game_board[y][x];
-
-    if (actual_status === 2 || actual_status === 3) { return false; }
-    game_board[y][x] = (actual_status === 1) ? 2 : 3;
-
-    render_board();
-}
-
-const fire_input = (fire = false) => {
-    if (!fire) {
-        show_modal();
-        return false;
-    }
+const ShowShips = () => {
+    ShowedShips = !ShowedShips; // Opposite value: if true, false; if false, true;
+    ShowShipsButton.innerHTML = ShowedShips ? "Show" : "Hide";
     
-    let coordinates = document.querySelector("#fire-positions").value;
-    if (coordinates.length === 0) { return false; }
+    RenderBoard();
+}
 
-    coordinates = coordinates.split(",");
+const FireShip = (y, x) => {
+    const SquareStatus = GameBoard[y][x];
+
+    if (SquareStatus === sunkenPart || SquareStatus === missingShot) { return false; }
+    GameBoard[y][x] = (SquareStatus === shipPart) ? sunkenPart : missingShot;
+
+    RenderBoard();
+}
+
+const FireInput = (confirmation = false) => {    
+    if (!confirmation) { 
+        ModalXCoordinateInput.value = null;
+        ModalYCoordinateInput.value = null;
+        ShowModal(FireModal, true); 
+        return false; 
+    }
+
+    let coordinates = [ModalYCoordinateInput.value, ModalXCoordinateInput.value];
+    coordinates = coordinates.filter(coordinate => coordinate > 0 && coordinate < 10);
+
     if (coordinates.length !== 2) { return false; }
-
-    let x = coordinates[0].trim();
-    let y = coordinates[1].trim();
-
-    if (isNaN(x) || isNaN(y)) { return false; }
-    if (x > 0 && x < 10 && y > 0 && y < 10) {
-        shoot([parseInt(y), parseInt(x)]); // In this moment, input have a comma separated numbers    
-        show_modal(false);
-    }
-}
-
-const check_remaining_parts = () => {
-    let sunked_count = game_board.map(rows => rows.filter(cell_value => cell_value === 2).length).reduce((x, y) => x + y);
-    remaining_parts.innerHTML = total_parts - sunked_count;
-
-    if (sunked_count === total_parts) {
-        game_result.innerHTML = "YOU WIN!!";
-        game_result.style.display = "block";
-
-        document.querySelectorAll(".square").forEach(div => div.classList.add("block"));
-    }
-}
-
-const show_modal = (show = true) => {
-    if (show) {
-        fire_modal.style.display = "block";
-        return false;
-    }
     
-    fire_modal.style.display = "none";
+    FireShip(coordinates[0] - 1, coordinates[1] - 1); // We must substract 1 because the user wil insert numbers from 1 to 9 and array indexes begints at 0
+    ShowModal(FireModal, false);
 }
 
-window.onload = () => render_board();
+const ShowModal = (modal, show) => {
+    modal.style.display = show ? "block" : "none";
+}
+
+const ShowUserNotifications = (show = false) => {
+    if (!show) { GameUserNotifications.style.display = "none";  return false; }
+
+    const RandomColor = "#" + Math.floor(Math.random() * 16777215).toString(16); // https://gist.github.com/hafizhrf/95eeb5bba6c5fe48fd54fbea0cf0eda0
+    GameUserNotifications.style.color = RandomColor;
+    GameUserNotifications.style.display = "block";
+}
+
+const SetUserNotifications = (notification) => {
+    GameUserNotifications.innerHTML = notification;
+    ShowUserNotifications(true);
+}
+
+const FinishedGame = () => {
+    document.querySelectorAll(".square").forEach(div => div.classList.add("block")); // Disable events in all game squares
+}
+// #endregion FUNCTIONS
+
+window.onload = () => InitApp();
